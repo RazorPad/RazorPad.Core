d28593ca5678488658874799878f26c8fb42b6ad
Hrusikesh Panda <mrchief_2000@yahoo.com>
2011-07-17 23:38:19 -0400

    Initial Clone

---
+++ src/RazorPad.Website/Controllers/MockController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;   
+
+namespace RazorPad.Website.Controllers
+{
+    public class MockController : Controller
+    {
+        //
+        // GET: /Mock/
+
+        public ActionResult Mock()
+        {
+            return View();
+        }
+
+    }
+}
