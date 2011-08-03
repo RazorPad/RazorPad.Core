5c96dd01bba6f54937feb818c9457b1344530e14
Hrusikesh Panda <mrchief_2000@yahoo.com>
2011-08-03 15:17:26 -0400

    layout enhancements, custom tab plugin

---
+++ src/RazorPad.Website/Scripts/hpTabs.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $.fn.extend({
+        hpTabs: function (options) {
+            var defaults = {},
+                    options = $.extend(defaults, options);
+
+            var ShowTab = function (lnk) {
+                var $lnk = $(lnk),
+    	            $panel = $($lnk.attr("href"));
+                $lnk.siblings("a").removeClass("selected");
+                $panel.siblings("div").hide();
+                $lnk.addClass("selected");
+                $panel.show();
+            };
+
+            return this.each(function () {
+                var o = options;
+                var $this = $(this);
+                $this.find(".tabBar > a").click(function () {
+                    ShowTab(this);
+                    return false;
+                });
+                ShowTab($this.find(".tabBar > a")[0]);
+            });
+        }
+    });
+} (jQuery))
