0f4d560f0d2ddeba784a753cae5927a495945199
Jess Chadwick <jesschadwick@gmail.com>
2012-02-17 00:12:10 -0500

    Moving src to root

--- src/RazorPad.Website/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RazorPad.Website.MvcApplication" Language="C#" %>
--- src/RazorPad.Website/Models/ParseRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RazorPad.Website.Models
-{
-    public class ParseRequest
-    {
-        public string Template { get; set; }
-    }
-}
