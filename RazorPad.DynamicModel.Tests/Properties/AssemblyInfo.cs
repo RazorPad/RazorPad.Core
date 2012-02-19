d3227859d948c34418aa15294511249846ac0538
mrchief <mrchief_2000@yahoo.com>
2012-02-19 01:57:56 -0500

    Added JsonDynamicObject to serialize json into dynamic objects Added test project

---
+++ RazorPad.DynamicModel.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RazorPad.DynamicModel.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("home")]
+[assembly: AssemblyProduct("RazorPad.DynamicModel.Tests")]
+[assembly: AssemblyCopyright("Copyright © home 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cd7ca84-bbe6-4061-b405-2165f0378416")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RazorPad.DynamicModel/JsonDynamicObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace RazorPad.DynamicModel
+{
+    public class JsonDynamicObject : DynamicObject
+    {
+        private IDictionary<string, object> _properties;
+
+        public JsonDynamicObject(IDictionary<string, object> dictionary)
+        {
+            _properties = dictionary;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = _properties[binder.Name];
+
+            if (result is IDictionary<string, object>)
+            {
+                result = new JsonDynamicObject(result as IDictionary<string, object>);
+            }
+            else if (result is ArrayList && (result as ArrayList) is IDictionary<string, object>)
+            {
+                result = new List<JsonDynamicObject>((result as ArrayList).ToArray().Select(x => new JsonDynamicObject(x as IDictionary<string, object>)));
+            }
+            else if (result is ArrayList)
+            {
+                result = new List<object>((result as ArrayList).ToArray());
+            }
+
+            return _properties.ContainsKey(binder.Name);
+        }
+
+    }
+}
