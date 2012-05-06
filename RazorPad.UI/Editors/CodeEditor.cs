6f0c363e12e2ddc659a3959df9be7b66836a88dd
mrchief <mrchief_2000@yahoo.com>
2012-05-06 02:21:23 -0400

    AvalonEdit, 2-way binding, delay text notifier and all that jazz!

---
+++ RazorPad.UI/Editors/CodeEditor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Folding;
+
+namespace RazorPad.UI.Editors
+{
+    public class CodeEditor : UserControl
+    {
+        public bool SuspendEditorUpdate { get; private set; }
+        public TextEditor Editor { get; private set; }
+
+        private DispatcherTimer _textChangedTimer;
+
+        public CodeEditor()
+        {
+            InitializeEditor();
+            InitializeTextChangedTimer();
+        }
+
+        private void InitializeEditor()
+        {
+            Editor = new TextEditor
+                             {
+                                 ShowLineNumbers = true,
+                                 FontFamily = new FontFamily("Consolas"),
+                                 FontSize = (double)(new FontSizeConverter().ConvertFrom("10pt") ?? 10.0)
+                             };
+
+            Editor.KeyUp += (sender, args) =>
+            {
+                if (_textChangedTimer.IsEnabled)
+                    _textChangedTimer.Stop();
+
+                _textChangedTimer.Start();
+
+            };
+            AddChild(Editor);
+        }
+
+        private void InitializeTextChangedTimer()
+        {
+            _textChangedTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+            _textChangedTimer.Tick += (sender, args) =>
+            {
+                //Debug.Print("codeEditor: TimerTick");
+                SuspendEditorUpdate = true;
+                Text = Editor.Text;
+                SuspendEditorUpdate = false;
+            };
+        }
+
+        protected void InitializeFolding(AbstractFoldingStrategy foldingStrategy)
+        {
+            var foldingManager = FoldingManager.Install(Editor.TextArea);
+            foldingStrategy.UpdateFoldings(foldingManager, Editor.Document);
+
+            var foldingUpdateTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            foldingUpdateTimer.Tick += (o, args) => foldingStrategy.UpdateFoldings(foldingManager, Editor.Document);
+
+            foldingUpdateTimer.Start();
+        }
+
+        #region TextProperty
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static void OnTextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //Debug.Print("codeEditor: OnTextChanged");
+            var codeEditor = sender as CodeEditor;
+
+            if (!codeEditor.SuspendEditorUpdate)
+                codeEditor.Editor.Text = (e.NewValue ?? string.Empty).ToString();
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text",
+            typeof(String), typeof(CodeEditor), new FrameworkPropertyMetadata(string.Empty, OnTextChanged)
+            {
+                BindsTwoWayByDefault = true,
+            }); 
+
+        #endregion
+    }
+}
---
+++ RazorPad.UI/Editors/RazorTemplateEditor.cs
@@ -0,0 +1,17 @@
+﻿using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Indentation;
+
+namespace RazorPad.UI.Editors
+{
+	public class RazorTemplateEditor : CodeEditor
+	{
+		public RazorTemplateEditor()
+		{
+            Editor.TextArea.IndentationStrategy = new DefaultIndentationStrategy();
+			InitializeFolding(new XmlFoldingStrategy());
+		    Editor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("HTML");
+		}
+
+	}
+}
